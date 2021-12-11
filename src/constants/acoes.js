95d9eb225309852af5ae463113325bd1803b8ea1
Luan Rodrigues <luan.rodrigues.dev@hotmail.com>
2021-12-11 19:41:46 -0300

    Refatorei calculo do proximo estado de acordo com acao

--- src/constants/acoes.js
+++ src/constants/acoes.js
@@ -1,7 +1,15 @@
+class Acao {
+    constructor(canibal, missionario) {
+        this.qnt_canibal = canibal;
+        this.qnt_missioanario = missionario;
+    }
+}
+
+
 export default {
-    UM_CANIBAL: 'UM CANIBAL',
-    UM_MISSIONARIO: 'UM MISSIONARIO',
-    DOIS_CANIBAIS: 'DOIS CANIBAIS',
-    DOIS_MISSIONARIOS: 'DOIS MISSIONARIOS',
-    UM_CANIBAL_UM_MISSIONARIO: 'UM CANIBAL E UM MISSIONARIO'
+    UM_CANIBAL: new Acao(1, 0),
+    UM_MISSIONARIO: new Acao(0, 1),
+    DOIS_CANIBAIS: new Acao(2, 0),
+    DOIS_MISSIONARIOS: new Acao(0, 2),
+    UM_CANIBAL_UM_MISSIONARIO: new Acao(1, 1)
 };
--- src/jogo/Margem.js
+++ src/jogo/Margem.js
@@ -1,6 +1,3 @@
-import acoes from '../constants/acoes';
-
-
 class Margem {
     constructor({ canibais, missionarios }) {
         this.canibais = canibais;
@@ -8,40 +5,23 @@ class Margem {
     }
 
     pode_executar_acao(acao) {
-        let canibais_restantes = this.canibais;
-        let missioanrios_restantes = this.missionarios;
+        const proximo_estado = this._proximo_estado_dada_acao(acao);
 
-        switch (acao) {
-            case acoes.UM_CANIBAL:
-                canibais_restantes--;
-                break;
-            case acoes.UM_MISSIONARIO:
-                missioanrios_restantes--;
-                break;
-            case acoes.UM_CANIBAL_UM_MISSIONARIO:
-                missioanrios_restantes --;
-                canibais_restantes --;
-                break;
-            case acoes.DOIS_CANIBAIS:
-                canibais_restantes -= 2;
-                break;
-            case acoes.DOIS_MISSIONARIOS:
-                missioanrios_restantes -= 2;
-                break;
-            default:
-                missioanrios_restantes = -1;
-                canibais_restantes = -1;
-                break;
-        };
-
-        const missionarios_sobrevivem = missioanrios_restantes >= canibais_restantes;
-        const quantidade_negativa = missioanrios_restantes < 0 || canibais_restantes < 0;
+        const missionarios_sobrevivem = proximo_estado.missioanrios >= proximo_estado.canibais;
+        const quantidade_negativa = proximo_estado.missioanrios < 0 || proximo_estado.canibais < 0;
 
         return missionarios_sobrevivem && !quantidade_negativa;
     }
 
     esta_vazia() {
         return this.canibais === 0 && this.missionarios === 0;
+    }
+
+    _proximo_estado_dada_acao(acao) {
+        return {
+            canibais: this.canibais - acao.qnt_canibal, 
+            missioanrios: this.missionarios - acao.qnt_missioanario
+        };
     }
 };
 
