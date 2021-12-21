bf466b2462fbacb8c501e197a79eea301f506b59
Luan Rodrigues <luan.rodrigues.dev@hotmail.com>
2021-12-20 23:06:47 -0300

    Removendo os lixos

--- src/front/Lugares.js
+++
@@ -1,8 +0,0 @@
-const lugares = {
-  canoaDireita: [66.4, 340],
-  canoaEsquerda: [66.4, 650],
-  lugarNaCanoaDireita: [[96, 480], [96, 384]],
-  lugarNaCanoaEsquerda: [[96, 800], [96, 704]],
-};
-
-export default lugares
